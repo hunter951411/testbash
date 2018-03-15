c8c28e1790a43223c1a4af23cfe6264502c61ab1
Nguyễn Quốc Trung - KMA <nguyenquoctrung141195@gmail.com>
2018-03-16 01:53:48 +0700

    Create hello_edumall.sh

---
+++ hello_edumall.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X POST --data-urlencode "payload={\"channel\": \"#tst_attack_kube_alert\", \"username\": \"Mchan\", \"text\": \"This is posted to #tst_attack_kube_alert and comes from a bot named webhookbot.\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
